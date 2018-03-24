a6d3c832a483b70224f3c7aaded3795df7105a4b
Edderic Ugaddan <edderic@gmail.com>
2018-03-24 18:36:30 -0400

    cleanup/refactor

--- sketch_feb17a/sketch_feb17a.ino
+++ sketch_feb17a/sketch_feb17a.ino
@@ -19,21 +19,70 @@ uint8_t buf[8] = {
     bit 7: right GUI
 */
 
-#define KEY_ONE     0x04 /* 1 */
-#define KEY_TWO     0x1F
-#define KEY_THREE   0xE1
-#define KEY_FOUR    0x21
+// Modifier values
+#define KEY_LEFT_CONTROL  0xE0
+#define KEY_LEFT_SHIFT    0xE1
+#define KEY_LEFT_ALT      0xE2
+#define KEY_LEFT_GUI      0xE3
+#define KEY_RIGHT_CONTROL 0xE4
+#define KEY_RIGHT_SHIFT   0xE5
+#define KEY_RIGHT_ALT     0xE6
+#define KEY_RIGHT_GUI     0xE7
 
-#define PIN_ONE     4
-#define PIN_TWO     5
-#define PIN_THREE   6
-#define PIN_FOUR    7
+// Order matters
+// Modifier pins
+#define PIN_LEFT_CONTROL  2
+#define PIN_LEFT_SHIFT    3
+#define PIN_LEFT_ALT      4
+#define PIN_LEFT_GUI      5
+#define PIN_RIGHT_CONTROL 6
+#define PIN_RIGHT_SHIFT   7
+#define PIN_RIGHT_ALT     8
+#define PIN_RIGHT_GUI     9
 
-int leftShiftState = 1;
+// TODO: w
+// Non modifier values
+#define KEYBOARD_7_AND_AMPERSAND  0x24
 
-int state = 1;
-int state1 = 1;
-int state2 = 1;
+// Non modifier pin numbers
+#define PIN_KEYBOARD_7_AND_AMPERSAND  10
+
+int modifierInputs[] = {
+  PIN_LEFT_CONTROL,
+  PIN_LEFT_SHIFT,
+  PIN_LEFT_ALT,
+  PIN_LEFT_GUI,
+  PIN_LEFT_CONTROL,
+  PIN_LEFT_SHIFT,
+  PIN_LEFT_ALT,
+  PIN_LEFT_GUI,
+};
+
+int modifierValues[] = {
+  KEY_LEFT_CONTROL,
+  KEY_LEFT_SHIFT,
+  KEY_LEFT_ALT,
+  KEY_LEFT_GUI,
+  KEY_LEFT_CONTROL,
+  KEY_LEFT_SHIFT,
+  KEY_LEFT_ALT,
+  KEY_LEFT_GUI,
+};
+
+int regularInputs[] = {
+  PIN_KEYBOARD_7_AND_AMPERSAND
+};
+
+int regularValues[] = {
+  KEYBOARD_7_AND_AMPERSAND
+};
+
+int numModifierInputs = sizeof(modifierInputs) / sizeof(int);
+int numRegularInputs = sizeof(regularInputs) / sizeof(int);
+
+int modifierStates[8];
+int regularStates[100];
+
 
 void setup()
 {
@@ -41,50 +90,39 @@ void setup()
   randomSeed(analogRead(0));
   delay(200);
 
-  pinMode(PIN_ONE, INPUT);
-  pinMode(PIN_TWO, INPUT);
-  pinMode(PIN_THREE, INPUT);
-  pinMode(PIN_FOUR, INPUT);
+  for(int i = 0; i < numModifierInputs; i++) {
+    pinMode(modifierInputs[i], INPUT);
+    digitalWrite(modifierInputs[i], 1);
+  }
 
-  digitalWrite(PIN_ONE, 1);
-  digitalWrite(PIN_TWO, 1);
-  digitalWrite(PIN_THREE, 1);
-  digitalWrite(PIN_FOUR, 1);
+  for(int i = 0; i < numRegularInputs; i++) {
+    pinMode(regularInputs[i], INPUT);
+    digitalWrite(regularInputs[i], 1);
+  }
 }
 
 void loop()
 {
 
-  leftShiftState = digitalRead(PIN_ONE);
-
-  state = digitalRead(PIN_TWO);
-  if (state != 1) {
-    buf[0] = modifier(leftShiftState);
-    buf[2] = KEY_TWO;
-    Serial.write(buf, 8);
-    releaseKey();
-    delay(200);
+  // modifiers don't need to send info right away
+  // others need to send info
+  for(int i = 0; i < numModifierInputs; i++) {
+    modifierStates[i] = digitalRead(modifierInputs[i]);
   }
 
+  int modifierDecimal = getModifierDecimal();
 
-  state2 = digitalRead(PIN_THREE);
-  if (state2 != 1) {
-    buf[0] = modifier(leftShiftState);
-    buf[2] = KEY_ONE;
-    Serial.write(buf, 8);
-    releaseKey();
-    delay(200);
+  for(int i = 0; i < numRegularInputs; i++) {
+    regularStates[i] = digitalRead(regularInputs[i]);
+
+    if (regularStates[i] != 1) {
+      buf[0] = modifierDecimal; // debug
+      buf[2] = regularValues[i];
+      Serial.write(buf, 8);
+      releaseKey();
+      delay(200);
+    }
   }
-
-  state = digitalRead(PIN_FOUR);
-  if (state != 1) {
-    buf[0] = modifier(leftShiftState);
-    buf[2] = KEY_FOUR;
-    Serial.write(buf, 8);
-    releaseKey();
-    delay(200);
-  }
-
 }
 
 void releaseKey()
@@ -94,13 +132,24 @@ void releaseKey()
   Serial.write(buf, 8); // Release key
 }
 
-int modifier(int leftShiftState)
+// Get the combination of modifier values, distilled into a single number
+int getModifierDecimal()
 {
-  if (leftShiftState != 1) {
-    return 2;
-  } else {
-    return 0;
+
+  int total = 0;
+  int multiplier = 1;
+
+  for(int i = 0; i < numModifierInputs; i++) {
+    if (i != 0) {
+      multiplier *= 2;
+    }
+
+    if (modifierStates[i] != 1) {
+      total += multiplier;
+    }
   }
+
+  return total;
 }
 
 
